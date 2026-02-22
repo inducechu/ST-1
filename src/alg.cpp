cdcbea0132c33f1f566e068c48952ea2d5817fe5
Induce <aa5619721@gmail.com>
2026-02-22 16:32:10 +0300

    iteration 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,52 @@
 // Copyright 2025 UNN-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <cstdint>
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1)
+    return false;
+  if (value <= 3)
+    return true;
+  if (value % 2 == 0 || value % 3 == 0)
+    return false;
+  for (uint64_t k = 5; (k * k) <= value; k = (k + 6)) {
+    if (value % k == 0 || value % (k + 2) == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0)
+    return 0;
+  uint64_t cnt = 0;
+  uint64_t curr = 1;
+  while (cnt < n) {
+    curr++;
+    if (checkPrime(curr)) {
+      cnt++;
+    }
+  }
+  return curr;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  if (value < 2)
+    return 2;
+
+  do {
+    value++;
+  } while (!checkPrime(value));
+
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i))
+      sum += i;
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -15,3 +15,97 @@ TEST(st1, sumPrime2) {
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
 }
+TEST(st1, checkPrime3) {
+  bool res = checkPrime(2);
+  EXPECT_TRUE(res);
+}
+
+TEST(st1, checkPrime4) {
+  bool res = checkPrime(3);
+  EXPECT_TRUE(res);
+}
+
+TEST(st1, checkPrime5) {
+  bool res = checkPrime(4);
+  EXPECT_FALSE(res);
+}
+
+TEST(st1, checkPrime6) {
+  bool res = checkPrime(5);
+  EXPECT_TRUE(res);
+}
+
+TEST(st1, checkPrime7) {
+  bool res = checkPrime(97);
+  EXPECT_TRUE(res);
+}
+
+TEST(st1, checkPrime8) {
+  bool res = checkPrime(100);
+  EXPECT_FALSE(res);
+}
+
+TEST(st1, nPrime1) {
+  uint64_t res = nPrime(1);
+  EXPECT_EQ(2, res);
+}
+
+TEST(st1, nPrime2) {
+  uint64_t res = nPrime(2);
+  EXPECT_EQ(3, res);
+}
+
+TEST(st1, nPrime3) {
+  uint64_t res = nPrime(3);
+  EXPECT_EQ(5, res);
+}
+
+TEST(st1, nPrime4) {
+  uint64_t res = nPrime(4);
+  EXPECT_EQ(7, res);
+}
+
+TEST(st1, nPrime5) {
+  uint64_t res = nPrime(5);
+  EXPECT_EQ(11, res);
+}
+
+TEST(st1, nPrime6) {
+  uint64_t res = nPrime(10);
+  EXPECT_EQ(29, res);
+}
+
+TEST(st1, nPrime7) {
+  uint64_t res = nPrime(15);
+  EXPECT_EQ(47, res);
+}
+
+TEST(st1, nextPrime1) {
+  uint64_t res = nextPrime(0);
+  EXPECT_EQ(2, res);
+}
+
+TEST(st1, nextPrime2) {
+  uint64_t res = nextPrime(1);
+  EXPECT_EQ(2, res);
+}
+
+TEST(st1, nextPrime3) {
+  uint64_t res = nextPrime(2);
+  EXPECT_EQ(3, res);
+}
+
+TEST(st1, nextPrime4) {
+  uint64_t res = nextPrime(3);
+  EXPECT_EQ(5, res);
+}
+
+TEST(st1, nextPrime5) {
+  uint64_t res = nextPrime(4);
+  EXPECT_EQ(5, res);
+}
+
+TEST(st1, nextPrime6) {
+  uint64_t res = nextPrime(5);
+  EXPECT_EQ(7, res);
+}
